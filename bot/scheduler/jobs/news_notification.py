8a1fc860889f99309bdbadcb601c13aef16203b5
mvstronov <mvstronov@mts.ru>
2024-06-04 01:49:31 +0300

    добавлено изменение цены

--- bot/scheduler/jobs/news_notification.py
+++ bot/scheduler/jobs/news_notification.py
@@ -1,3 +1,5 @@
+import datetime
+from datetime import datetime, timedelta
 import time
 from typing import List
 
@@ -6,8 +8,9 @@ from aiogram import Bot
 from tinkoff.invest import AsyncClient
 from tinkoff.invest.grpc.common_pb2 import INSTRUMENT_TYPE_SHARE
 from tinkoff.invest.grpc.instruments_pb2 import FindInstrumentResponse, InstrumentShort
+from tinkoff.invest.schemas import Trade
 
-from bot.core.config import settings
+from bot.core.config import settings, TZ
 from bot.keyboards.inline.stocks import buy_sell_kb
 from bot.services.news_parser import get_last_news
 
@@ -39,20 +42,32 @@ async def send_news(bot: Bot, user_id: int, token: int):
                                        f"Failed to find ticker from tags:\n"
                                        f"Tags: {x['tags']};\n"
                                        f"Title: {x['title']}\n"
-                                       f"Content: {x['content']}\n"
+                                       f"Content: {' '.join(x['content'])}\n"
                                        f"Link: {x['link']}\n"
                                        f"ML Score: In work :(\n"
                                        f"Time elapsed: {e - s}")
             else:
                 ticker_str = " #".join([x.ticker for x in instruments_list])
                 name_str = " #".join([x.name for x in instruments_list])
+                market_data: List[List[Trade]] = [x.trades for x in
+                                                  await client.market_data.get_last_trades(instrument_id=a.uid)
+                                                  for a in instruments_list]
+                now = datetime.now(TZ)
+                prices_prev_min = [[x.price for x in trade] for trade in market_data if
+                                   now - timedelta(minutes=1) < x.time < now - timedelta(seconds=30)]
+                prices_now = [[x.price for x in trade ]for trade in market_data if
+                              now - timedelta(seconds=30) <= x.time]
+                avg_prev = [sum(x)/len(x) for x in prices_prev_min]
+                avg_new = [sum(x)/len(x) for x in prices_now]
+                delta = [round((b-a)/b*100, 2) for a, b in zip(avg_prev, avg_new)]
                 e = time.time()
                 await bot.send_message(user_id,
                                        f"#{ticker_str} \n"
                                        f"#{name_str} \n"
+                                       f"Price change: {delta}\n"
                                        f"Tags: {x['tags']};\n"
                                        f"Title: {x['title']}\n"
-                                       f"Content: {x['content']}\n"
+                                       f"Content: {' '.join(x['content'])}\n"
                                        f"Link: {x['link']}\n"
                                        f"ML Score: In work :(\n"
                                        f"Time elapsed: {e - s}",
